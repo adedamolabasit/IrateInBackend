15d8f2835f518d5767dabf78546219c5e1abc8bb
adedamolabasit <adedamolabasit09@gmail.com>
2024-01-07 03:36:03 +0100

    feat:whitelist vercel app

--- IrateInBackend/settings.py
+++ IrateInBackend/settings.py
@@ -57,7 +57,8 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "irate-in-backend.vercel.app"
+    "irate-in-backend.vercel.app",
+    "irate-in-backend-7ngnbsvtt-adedamolabasit.vercel.app"
     'http://localhost:5173',
     "http://127.0.0.1:8000",
     "http://localhost:3000",  
